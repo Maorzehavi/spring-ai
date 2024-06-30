7c6b3dab598b89372174e339caf338db667d731d
Maorz <maorzehavi@outlook.com>
2024-06-30 10:54:23 +0300

    adding api-key

--- src/main/java/com/maorzehavi/springai/SpringAiApplication.java
+++ src/main/java/com/maorzehavi/springai/SpringAiApplication.java
@@ -2,12 +2,21 @@ package com.maorzehavi.springai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiApplication.class, args);
     }
 
+
+@GetMapping
+public String hello() {
+    return "Hello, Spring AI!";
 }
+}
+
